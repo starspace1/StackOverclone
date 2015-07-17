1604ed09487f95dad340d6bfc351c0e152ec9560
Leslie Brown <lkb@alum.mit.edu>
2015-07-17 14:09:05 -0400

    Created seed data!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :posts
+  has_many :questions
+  has_many :answers
   has_many :comments
   validates_presence_of :username, :password_digest, :location, :knowledge
   validates_uniqueness_of :username
